3b95c0b130a9d5d99cd63e68268877f0d1510d6a
James Balamuta <balamut2@illinois.edu>
2020-04-04 13:10:11 -0500

    Add massmail download script

---
+++ 01-setup-mass-mail.R
@@ -0,0 +1,127 @@
+# Load dependencies ----
+library("rvest")
+library("tidyverse")
+
+# Non-tabular massmail parse rules ---- 
+
+# General table rule
+massmail_table_col = function(column_id, massmail_page) {
+  massmail_page %>%
+    html_nodes(glue::glue(".col{column_id}")) %>%
+    html_text()
+}
+
+# Email column specific rule
+massmail_table_col_email = function(massmail_page) {
+  massmail_page %>%
+    html_nodes(".col3 a") %>%
+    html_attr("href")
+}
+
+
+# Massmail contents retrieval and caching ----
+massmail_download_email = function(massmail_email_url,
+                                   save_dir = "massmail-emails") {
+  dir.create(save_dir, showWarnings = FALSE)
+  
+  # More control
+  for (url in massmail_email_url) {
+    file_loc = file.path(save_dir, basename(url))
+    
+    if (file.exists(file_loc))
+      next
+    
+    download.file(url, file_loc)
+  }
+}
+
+massmail_read_email = function(massmail_email_url,
+                               save_dir = "massmail-emails") {
+  
+  file_loc = file.path(save_dir, basename(massmail_email_url))
+  
+  # There really are about 4 templates across a 10 year span.
+  massmail_email_body = function(path) {
+    email = read_html(path) 
+    
+    message("Attempting approach one on ", path, " ...")
+    approach_one = email %>%
+      html_node("#wrapper > tbody > tr:nth-child(2) > td:nth-child(3)") %>%
+      html_text(trim = TRUE)
+    
+    if(!is.na(approach_one) && !identical(approach_one, "")) return(approach_one)
+    message("Attempting approach two on ", path, " ...")
+    
+    # Based off of 27144.html
+    approach_two = email %>%
+      html_node("#wrapper > tbody > tr:nth-child(2) > td:nth-child(2)") %>%
+      html_text(trim = TRUE)
+    
+    if(!is.na(approach_two) && !identical(approach_two, "")) return(approach_two)
+    
+    message("Attempting approach three on ", path, " ...")
+    
+    # Based off of 26844.html
+    approach_three = email %>%
+      html_node("table:nth-child(2) > tr:nth-child(1) ") %>%
+      html_text(trim = TRUE)
+    
+    if(!is.na(approach_three) && !identical(approach_three, "")) return(approach_three)
+    
+    message("Attempting approach four on ", path, " ...")
+    
+    # Based off of 26839.html
+    approach_four = email %>%
+      html_node("table:nth-child(3) tr:nth-child(1)") %>%
+      html_text(trim = TRUE)
+    
+    approach_four
+    
+   }
+  
+  map_chr(file_loc, massmail_email_body)
+}
+
+
+# Construct the massmail table ----
+massmail_meta_data = function(massmail_page) {
+  purrr::map_dfc(
+    c(
+      "date/time" = 1,
+      "sent" = 2,
+      "subject" = 4
+    ),
+    massmail_table_col,
+    massmail_page = massmail_page
+  ) %>%
+    mutate(`url` = massmail_table_col_email(massmail_page),
+           `date/time` = lubridate::mdy_hm(`date/time`))
+}
+
+# Handle non-table semantics
+massmail_table = function(massmail_page) { 
+  
+  # Form overview of data
+  massmail_meta_table = massmail_meta_data(massmail_page)
+  
+  # Download
+  massmail_meta_table %>% 
+    pull(url) %>%
+    massmail_download_email()
+  
+  # Pull e-mail contents 
+  contents = massmail_meta_table %>% 
+    pull(url) %>%
+    massmail_read_email()
+  
+  # Release table with emails attached
+  massmail_meta_table %>%
+    mutate(contents = contents)
+}
+
+
+# Build the massmail data table ----
+massmail_page = read_html("https://massmail.illinois.edu/massmailArchive")
+massmail_data = massmail_table(massmail_page)
+
+View(massmail_data)
