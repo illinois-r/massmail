3a259e3ef856ecbe4a1f2ba8fad2a7eef2a3dbcc
James Balamuta <balamut2@illinois.edu>
2021-02-01 18:11:11 -0600

    - Fix facetting with gghighlight by avoiding showing the subset variable on subsequent plots (e.g. keep 2020 data on 2020 facet and 2021 data only on 2021's facet.) - Fix the axis tick text overlapping by rotating 45 degrees.

--- R/03-ggplot-covid19-msg.R
+++ R/03-ggplot-covid19-msg.R
@@ -3,18 +3,23 @@ theme_set(theme_bw())
 
 ggplot(massmail_data_covid) +
   geom_bar(aes(`date`, fill = covid19)) +
-  gghighlight(covid19, use_direct_label = FALSE) +
+  gghighlight(covid19, use_direct_label = FALSE, calculate_per_facet = TRUE) +
   labs(
     title = "Number of Times COVID-19 Showed up in Massmails Sent",
     y = "Frequency",
     x = "Date"
-  ) + facet_wrap(~year(date))
+  ) + 
+  facet_wrap(~year(date), scales = "free_x") +
+  theme(
+    axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)
+  )
 
 ggplot(massmail_data_covid) +
   geom_bar(aes(weekday, fill = covid19)) +
-  gghighlight(covid19, use_direct_label = FALSE) +
+  gghighlight(covid19, use_direct_label = FALSE, calculate_per_facet = TRUE) +
   labs(
     title = "Weekdays When Massmails Were Sent with COVID-19 Information",
     y = "Frequency",
     x = "Day of Week"
-  )
+  ) + 
+  facet_wrap(~year(date))
